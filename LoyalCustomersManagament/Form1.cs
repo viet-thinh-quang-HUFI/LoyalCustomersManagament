206fd38204856f79ca50fa041dbd89f1991faf64
ThinhDN <thinhdn.hufi@gmail.com>
2023-10-12 01:54:16 +0700

    load dataGridView sanpham

--- LoyalCustomersManagament/Form1.cs
+++ LoyalCustomersManagament/Form1.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 
 namespace LoyalCustomersManagament
 {
@@ -20,6 +25,27 @@ namespace LoyalCustomersManagament
         private void btnThoat_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<SanPham> sanPhams = new List<SanPham>();   
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var db = server.GetDatabase("LoyalCustomersManagement");
+            var collection = db.GetCollection<BsonDocument>("SanPham");
+            foreach (BsonDocument document in collection.FindAll())
+            {
+                string ma = document["MaSP"].AsString;
+                string ten = document["TenSP"].AsString;
+                Int32 gia = document["Dongia"].AsInt32;
+                Int32 sl = document["Soluongton"].AsInt32;
+                string hang = document["Hang"].AsString;
+                SanPham sanPham = new SanPham(ma, ten,gia,sl,hang);
+                sanPhams.Add(sanPham);
+            }
+            dataGridView1.DataSource = sanPhams;
         }
     }
 }
