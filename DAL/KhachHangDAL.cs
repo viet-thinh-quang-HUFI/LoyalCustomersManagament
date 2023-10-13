ccc6e36fe1c8ef2844b02688868b95824944e8f6
ThinhDN <thinhdn.hufi@gmail.com>
2023-10-13 20:09:59 +0700

    load frmKhachHang

---
+++ DAL/KhachHangDAL.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class KhachHangDAL
+    {
+        KetNoi KetNoi = new KetNoi();
+        public KhachHangDAL() { }
+        public MongoCollection<BsonDocument> getKhachHang()
+        {
+            MongoCollection<BsonDocument> collection = KetNoi.GetAllCollection("KhachHang");
+            return collection;
+        }
+    }
+}
---
+++ GUI/frmKhachHang.cs
@@ -0,0 +1,27 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmKhachHang : Form
+    {
+        KhachHangBLL KhachHangBLL = new KhachHangBLL();
+        public frmKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKhachHang_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = KhachHangBLL.getKhachHang();
+        }
+    }
+}
