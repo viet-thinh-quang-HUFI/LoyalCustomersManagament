1a34b9bc92f95b9a7209d77d197d6409a457ca2c
ThinhDN <thinhdn.hufi@gmail.com>
2023-10-13 13:22:29 +0700

    merge into thinh

---
+++ GUI/frmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            CustomMenu();
+        }
+        private void CustomMenu()
+        {
+            panel1.Visible = false;
+            //panel2.Visible = false;
+        }
+        private void hideMenu()
+        {
+            if (panel1.Visible == true)
+                panel1.Visible = false;
+            //if (panel2.Visible == true)
+            //    panel2.Visible = false;
+        }
+        private void showMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showMenu(panel1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenForm(new frmSanPham());
+            hideMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hideMenu();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            hideMenu();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            hideMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenForm(new frmThongKe());
+            hideMenu();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            hideMenu();
+        }
+        private Form activeForm = null;
+        private void OpenForm(Form form)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = form;
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            panelForm.Controls.Add(form);
+            panelForm.Tag = form;
+            form.BringToFront();
+            form.Show();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int w = Screen.PrimaryScreen.Bounds.Width;
+            int h = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0,0);
+            this.Size = new Size(w, h);
+        }
+
+        
+    }
+}
---
+++ GUI/frmThongKe.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace GUI
+{
+    partial class frmThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgKhachHang = new System.Windows.Forms.DataGridView();
+            this.cbCapBac = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiem = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtNgayBD = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtNgayKT = new System.Windows.Forms.DateTimePicker();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgKhachHang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgKhachHang
+            // 
+            this.dtgKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgKhachHang.Location = new System.Drawing.Point(111, 244);
+            this.dtgKhachHang.Name = "dtgKhachHang";
+            this.dtgKhachHang.RowHeadersWidth = 51;
+            this.dtgKhachHang.RowTemplate.Height = 24;
+            this.dtgKhachHang.Size = new System.Drawing.Size(658, 383);
+            this.dtgKhachHang.TabIndex = 0;
+            // 
+            // cbCapBac
+            // 
+            this.cbCapBac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCapBac.FormattingEnabled = true;
+            this.cbCapBac.Location = new System.Drawing.Point(271, 102);
+            this.cbCapBac.Name = "cbCapBac";
+            this.cbCapBac.Size = new System.Drawing.Size(123, 33);
+            this.cbCapBac.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cấp bậc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(265, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(379, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Thống kê điểm khách hàng";
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(648, 173);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(121, 36);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Sắp xếp";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tên khách hàng";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Enabled = false;
+            this.txtTen.Location = new System.Drawing.Point(271, 173);
+            this.txtTen.Multiline = true;
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(325, 36);
+            this.txtTen.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(423, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Số điểm";
+            // 
+            // txtDiem
+            // 
+            this.txtDiem.Enabled = false;
+            this.txtDiem.Location = new System.Drawing.Point(526, 102);
+            this.txtDiem.Multiline = true;
+            this.txtDiem.Name = "txtDiem";
+            this.txtDiem.Size = new System.Drawing.Size(70, 36);
+            this.txtDiem.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(1187, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 32);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Thống kê hóa đơn";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(977, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(635, 383);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // dtNgayBD
+            // 
+            this.dtNgayBD.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtNgayBD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtNgayBD.Location = new System.Drawing.Point(1131, 108);
+            this.dtNgayBD.Name = "dtNgayBD";
+            this.dtNgayBD.Size = new System.Drawing.Size(200, 30);
+            this.dtNgayBD.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(975, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Ngày bắt đầu";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(972, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 25);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Ngày kết thúc";
+            // 
+            // dtNgayKT
+            // 
+            this.dtNgayKT.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtNgayKT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtNgayKT.Location = new System.Drawing.Point(1131, 184);
+            this.dtNgayKT.Name = "dtNgayKT";
+            this.dtNgayKT.Size = new System.Drawing.Size(200, 30);
+            this.dtNgayKT.TabIndex = 8;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKe.Location = new System.Drawing.Point(1470, 173);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(142, 41);
+            this.btnThongKe.TabIndex = 9;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            // 
+            // frmThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1646, 701);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.dtNgayKT);
+            this.Controls.Add(this.dtNgayBD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDiem);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCapBac);
+            this.Controls.Add(this.dtgKhachHang);
+            this.Name = "frmThongKe";
+            this.Text = "frmThongKe";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgKhachHang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgKhachHang;
+        private System.Windows.Forms.ComboBox cbCapBac;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dtNgayBD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtNgayKT;
+        private System.Windows.Forms.Button btnThongKe;
+    }
+}
