7d02755a85466520f2983d3613e3579fc0b2a45a
Thinh Dang <134244643+ThinhDN02@users.noreply.github.com>
2023-10-15 04:04:11 +0700

    thong ke theo ngay

---
+++ DTO/ndHoaDon.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ndHoaDon
+    {
+        private string _masp;
+        private int _sl;
+
+        [BsonElement("MaSP"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string MaSP { get => _masp; set => _masp = value; }
+
+        [BsonElement("Soluong"), BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int Sl { get => _sl; set => _sl = value; }
+    }
+}
