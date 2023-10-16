44c0234e6cce3d27e2b6c5d613d04fb3a3f39f9a
Thinh Dang <134244643+ThinhDN02@users.noreply.github.com>
2023-10-16 14:43:25 +0700

    fix frmKH

--- GUI/frmSanPham.cs
+++ GUI/frmSanPham.cs
@@ -55,7 +55,7 @@ namespace GUI
 
             string kq = sanPhamBLL.Them(txtMa.Text, txtTen.Text, txtGia.Text, txtSLT.Text, txtHang.Text);
             MessageBox.Show(kq);
-            dataGridView1.DataSource = sanPhamBLL.GetSanPham();
+            load();
         }
 
         private void frmSanPham_Load(object sender, EventArgs e)
@@ -67,11 +67,18 @@ namespace GUI
             //int start = 0;
             //int end = 2;
 
+            load();
 
+
+
+            //SanPhamBLL.GetMoTa("");
+        }
+        public void load()
+        {
             ProjectionDefinition<SanPham> simpleProjection = Builders<SanPham>.Projection
-                .Exclude(u => u.Id)
-                .Include(u => u.MaSP)
-                .Include(u => u.TenSP);
+               .Exclude(u => u.Id)
+               .Include(u => u.MaSP)
+               .Include(u => u.TenSP);
 
             PipelineDefinition<SanPham, BsonDocument> pipeline = new BsonDocument[]
             {
@@ -91,9 +98,6 @@ namespace GUI
             dataGridView1.DataSource = a;
             dataGridView1.Columns["id"].Visible = false;
             dataGridView1.Columns["Mota"].Visible = false;
-
-
-            //SanPhamBLL.GetMoTa("");
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
@@ -106,7 +110,7 @@ namespace GUI
             else
             {
                 sanPhamBLL.Xoa(ma);
-                dataGridView1.DataSource = sanPhamBLL.GetSanPham();
+                load();
             }
         }
 
@@ -114,7 +118,7 @@ namespace GUI
         {
             string kq = sanPhamBLL.Sua(txtMa.Text, txtTen.Text, txtGia.Text, txtSLT.Text, txtHang.Text);
             MessageBox.Show(kq);
-            dataGridView1.DataSource = sanPhamBLL.GetSanPham();
+            load();
         }
     }
 }
